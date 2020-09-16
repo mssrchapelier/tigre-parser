cefad2619e1eed91253b1efe80fa314e0be865c5
mssrchapelier <26167505+mssrchapelier@users.noreply.github.com>
2020-09-16 19:09:48 +0300

    renamed a local variable in WordGlossPairComparator.compare for clarity

--- TigreParser/src/WordGlossPairComparator.java
+++ TigreParser/src/WordGlossPairComparator.java
@@ -6,28 +6,31 @@ public class WordGlossPairComparator implements Comparator<WordGlossPair> {
 	
 	@Override
 	public int compare (WordGlossPair wgp_1, WordGlossPair wgp_2) {
+		// final_analysis > non-final_analysis
 		if (wgp_1.isFinalAnalysis && !wgp_2.isFinalAnalysis) {
 			return 1;
 		} else if (!wgp_1.isFinalAnalysis && wgp_2.isFinalAnalysis) {
 			return -1;
 		} else {
-			int numMorph_1;
-			int numMorph_2;
+			// both final or both non-final
+
+			int morphemeCount_1;
+			int morphemeCount_2;
 			Pattern p = Pattern.compile("\\-");
 			Matcher m = p.matcher(wgp_1.lexicalForm);
-			numMorph_1 = (m.find()) ? m.groupCount() + 1 : 1;
+			morphemeCount_1 = (m.find()) ? m.groupCount() + 1 : 1;
 			m = p.matcher(wgp_2.lexicalForm);
-			numMorph_2 = (m.find()) ? m.groupCount() + 1 : 1;
+			morphemeCount_2 = (m.find()) ? m.groupCount() + 1 : 1;
 			
 			if (wgp_1.isFinalAnalysis && wgp_2.isFinalAnalysis) {
-				// less morphemes => less than
-				if (numMorph_1 < numMorph_2) { return 1; }
-				else if (numMorph_1 == numMorph_2) { return 0; }
+				// both final: fewer_morphemes > more_morphemes
+				if (morphemeCount_1 < morphemeCount_2) { return 1; }
+				else if (morphemeCount_1 == morphemeCount_2) { return 0; }
 				else { return -1; }
 			} else {
-				// more morphemes => less than
-				if (numMorph_1 < numMorph_2) { return -1; }
-				else if (numMorph_1 == numMorph_2) { return 0; }
+				// both non-final: fewer_morphemes < more_morphemes
+				if (morphemeCount_1 < morphemeCount_2) { return -1; }
+				else if (morphemeCount_1 == morphemeCount_2) { return 0; }
 				else { return 1; }
 			}
 		}
